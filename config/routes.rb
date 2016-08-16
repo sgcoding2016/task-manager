8c8f33ba27b64be4019df7d42df50d8b1b3ee2ce
SaulGreenberg <sg@10past12.co>
2016-08-16 15:44:01 +0100

    completed challenge of task list

---
+++ config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tasks
+
+
+    # get '/tasks': to: "restaurants#index"
+    # get '/tasks/:id': to: "restaurants#show"
+    # get '/tasks/new': to: "restaurants#new"
+    # post '/tasks': to: "restaurants#create"
+    # get '/tasks/:id/edit': to: "restaurants#edit"
+    # patch '/tasks/:id': to: "restaurants#update"
+    # delete '/tasks/:id': to: "restaurants#destroy"
+
+
+
+
+end
